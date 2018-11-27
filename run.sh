1d9a54ca3e004b75b96f6485a5a66bade77a52ab
Pablo Caderno <pabloc@qualtrics.com>
2018-11-27 15:24:17 +0000

    deploying

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/ash
 
 cd /app/simple-sinatra-app/
-rm helloworld.rb
-cat << EOF > helloworld.rb
-require 'sinatra'
-get '/' do
-  "Hello World updated!"
-end
-EOF
+#rm helloworld.rb
+#cat << EOF > helloworld.rb
+#require 'sinatra'
+#get '/' do
+#  "Hello World updated!"
+#end
+#EOF
 rackup exec --host 0.0.0.0 config.ru
